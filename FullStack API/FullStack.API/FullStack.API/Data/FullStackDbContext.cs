f254a7a31b4f5758df1625c00c5ca0c9fb211715
Fernando Simoes <fsimoes99@gmail.com>
2023-04-02 18:27:07 +0100

    CRUD working

---
+++ FullStack API/FullStack.API/FullStack.API/Data/FullStackDbContext.cs
@@ -0,0 +1,36 @@
+﻿using FullStack.API.Models;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FullStack.API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        // override methos to use sqlite
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName=fullstackdb", option =>
+            {
+                option.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("Employee", "Test");
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey()
+            });
+            base.OnModelCreating(modelBuilder);
+        }*/
+    }
+}
